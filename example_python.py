a7861bcfcb03161a38ed4929c1dc70fcead4ee20
William Bout <iamwillou@gmail.com>
2016-01-05 18:33:11 +0100

    Update example_python.py

--- example_python.py
+++ example_python.py
@@ -4,11 +4,12 @@ import sys
 pin = 4
 sensor = Adafruit_DHT.DHT22
 
-f = open('/home/pi/dht/temp.txt', 'w')
-e = open('/home/pi/dht/hum.txt', 'w')
 num_humidity, num_temperature = Adafruit_DHT.read_retry(sensor, pin)
 txt_temperature = str(num_temperature)
 txt_humidity = str(num_humidity)
+
+f = open('/home/pi/dht/temp.txt', 'w')
+e = open('/home/pi/dht/hum.txt', 'w')
 e.write(txt_humidity)
 e.close()
 f.write(txt_temperature)
